f9fb423dc0d72c3847c08308e4cb2c577f55c65d
Brochb <brochbaltzer@gmail.com>
2023-08-27 13:34:27 -0600

    Began pseudo coding, added gitignore parameters

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Please input Project title.", "Please give a Description of the Project.",
+"Please provide Installation Instructions.", "Please Provide Usage Information.", 
+"Please provide Constribution Guidelines", "Please provide Test Instructions."];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file 
+function writeToFile(README.md, questions) {
+    //Title questions[0]
+    //Description questions[1]
+    //Table of Contents (clickable to navigate to relevant section)
+        //Contains the following: Instalation, Usage, License, Contributing, Tests, Questions
+    //Installation
+    //Usage
+    //License
+    //Constributing
+    //Tests
+    //Questions
+}
 
 // TODO: Create a function to initialize app
 function init() {}
