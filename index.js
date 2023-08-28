b65b1d78584d1bd0bd5f00b4a022a8949fd7a717
Brochb <brochbaltzer@gmail.com>
2023-08-28 16:23:16 -0600

    fleshed out initial code to collect user data

--- index.js
+++ index.js
@@ -5,18 +5,49 @@ const questions = ["Please input Project title.", "Please give a Description of 
 "Please provide Installation Instructions.", "Please Provide Usage Information.", 
 "Please provide Constribution Guidelines", "Please provide Test Instructions."];
 
-// TODO: Create a function to write README file 
-function writeToFile(README.md, questions) {
-    //Title questions[0]
-    //Description questions[1]
-    //Table of Contents (clickable to navigate to relevant section)
-        //Contains the following: Instalation, Usage, License, Contributing, Tests, Questions
-    //Installation
-    //Usage
-    //License
-    //Constributing
-    //Tests
-    //Questions
+// Function to write README file
+function writeToFile(fileName, data) {
+  const [title, description, installation, usage, contribution, tests] = data;
+
+  const readmeContent = `
+  # ${title}
+
+  ## Description
+  ${description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${installation}
+
+  ## Usage
+  ${usage}
+
+  ## Contributing
+  ${contribution}
+
+  ## Tests
+  ${tests}
+
+  ## Questions
+  If you have any questions, feel free to reach out via email or GitHub.
+
+  Email: your-email@example.com
+  GitHub: [Your GitHub Profile](https://github.com/your-github-username)
+  `;
+
+  fs.writeFile(fileName, readmeContent, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`README file (${fileName}) created successfully!`);
+    }
+  });
 }
 
 // TODO: Create a function to initialize app
